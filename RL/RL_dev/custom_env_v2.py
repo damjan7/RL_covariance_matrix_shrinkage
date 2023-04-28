83f0d5aee68c7baf3d7a4ba6a3749ef662de3c9a
Damjan Kostovic <damjan.kostovic@hotmail.com>
2023-04-28 16:12:35 +0200

    implementing new custom env

--- RL/RL_dev/custom_env_v2.py
+++ RL/RL_dev/custom_env_v2.py
@@ -35,14 +35,84 @@ from collections import deque
 
 class MyEnv(gym.Env):
 
-    def __init__(self):
+    def __init__(self, return_data_path, pf_size):
         super().__init__()
 
         # observation space;
         # = shrinkage intensity, and other factors?
         # can package it inside a gym.spaces.Dict
-
+        # maybe testing without 60_day vola first!
+        self.observation_space = spaces.Dict(
+            {
+                "shrinkage_intensity": spaces.Discrete(100),  # actually continuous
+                "60_day_vola": spaces.Discrete(100),  # would also be continuous actually
+             }
+        )
+        # OR maybe easier, as it would rather work with existing algos, 1 discrete space of (2, 100)..
 
         # action space should be my shrinkage intensity
         # for now discrete as my RL algos are written for discrete cases only
         self.action_space = spaces.Discrete(100)
+
+        # load the data into the environment
+        self.return_data_path = return_data_path
+        self.pf_size = pf_size
+        with open(rf"{self.return_data_path}\past_return_matrices_p{self.pf_size}.pickle", 'rb') as f:
+            self.past_return_matrices = pickle.load(f)
+
+        with open(rf"{self.return_data_path}\future_return_matrices_p{self.pf_size}.pickle", 'rb') as f:
+            self.future_return_matrices = pickle.load(f)
+
+        with open(rf"{self.return_data_path}\rebalancing_days_full.pickle", 'rb') as f:
+            self.rebalancing_days_full = pickle.load(f)
+
+
+    def reset(self):
+        '''
+        This method should return the first state in our environment
+        This could either be a random state, or just the first date in our dataset
+        for now: first date in our dataset
+
+        Also, it should create some stuff, such as rewards, weights, etc. to keep track of
+
+        Finally, it should reset the environment
+        '''
+        self.state = 0  # only index --> denotes the first entry of rebalancing_dates['actual_reb_days']
+                        # also makes a lot of sense since I always know "where I am"
+                        # or alternatively return a tuple containing state idx, and other vars..
+
+        self.rewards = []
+        self.done = False
+
+        return self.state, self.done  # or maybe do not return "done" but manually set it to False when resetting
+
+
+
+    def step(self, action):
+        '''
+        This method calculates new state and the reward obtained by the action
+        new state will just be next date
+        done = True if last date is reached
+
+        action = shrinkage intensity
+        '''
+
+        pass
+
+
+    ## train loop: take state --> return action
+    # state is only index in my case: hence the step function
+
+    def _get_obs_space(self):
+        """
+        returns observation space for a given state space ( =index)
+        """
+        # This can all be written more efficiently, but want to keep it simple and clear as a first step
+        date = self.rebalancing_days_full['actual_reb_day'].iloc[self.state]
+        past_return_data = self.past_return_matrices[self.state]
+        past_price_data = self.past_price_matrices[self.state]
+
+        # calc the stuff I want my rl agent to have, i.e., shrinkage_intensity, vola, some factors
+        # start small
+
+
